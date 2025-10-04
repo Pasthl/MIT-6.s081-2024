2033b888c642eff30e74171865339b81c8089521
Pasthl <jyhua@seas.upenn.edu>
2025-10-04 03:09:54 -0400

    complete sleep implementation

---
+++ user/sleep.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/stat.h"
+
+int main(int argc, char **argv){
+    if(argc < 2){
+        printf("Usage: sleep <ticks>\n");
+        exit(1);
+    }
+    sleep(atoi(argv[1]));
+    exit(0);
+}
